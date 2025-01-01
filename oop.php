df6f45a5b2748287608736e403332532ba5576a9
Parvez <pzhossain@gmail.com>
2025-01-02 00:46:10 +0600

    My changes

---
+++ oop.php
@@ -0,0 +1,107 @@
+<?php
+
+class Book {
+
+ // TODO: Add properties as Private
+
+ private $bookName;
+ private $copies;
+ 
+public function __construct($title, $availableCopies) {
+     // TODO: Initialize properties
+
+    $this -> bookName = $title;
+    $this -> copies = $availableCopies;
+     }
+
+// TODO: Add getTitle method
+public function getTitle (){
+    return $this -> bookName;
+}
+
+ // TODO: Add getAvailableCopies method
+public function getAvailableCopies (){
+    return $this -> copies;
+}
+ 
+ // TODO: Add borrowBook method
+public function borrowBook (){
+    if ($this -> copies > 0){
+        $this -> copies --;
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+ // TODO: Add returnBook method
+ public function returnBook (){
+        $this -> copies ++;
+    }
+}
+
+
+class Member {
+ // TODO: Add properties as Private
+private $memberName;
+
+ public function __construct($name) {
+      // TODO: Initialize properties
+      $this -> memberName = $name;
+     }
+
+ // TODO: Add getName method
+ public function getName (){
+    return $this -> memberName;
+ }
+ 
+ // TODO: Add borrowBook method
+ public function borrowBook ($book){
+    if ($book -> borrowBook ()){
+        echo $this -> memberName . "successfully borrow  '" . $book-> getTitle (). "'" . "</br>";
+    } else {
+        echo "$book-> getTitle () . is out of stock.";
+    }    
+ }
+
+ // TODO: Add returnBook method
+ public function returnBook ($book){
+    $book -> returnBook ();
+    echo $this -> memberName . "returned the book: " . $book -> getTitle () . "</br>";
+ }
+}
+
+// Usage
+// You have to create  2 books and 2 members. Members One should borrow  book 1 and Member Two should borrow  book 2.
+// TODO: Create 2 books with the following properties
+// Book 1 - Name: The Great Gatsby, Available Copies: 5.
+// Book 2 - Name: To Kill a Mockingbird, Available Copies: 3.
+ 
+$book1 = new Book ("The Great Gatsby", 5);
+$book2 = new Book ("To Kill a Mockingbird", 3); 
+
+// TODO: Create 2 members with the following properties
+// Member 1 - Name: John Doe
+// Member 2 - Name: Jane Smith
+
+$member1 = new Member ("John Doe");
+$member2 = new Member ("Jane Smith");
+
+ 
+// TODO: Apply Borrow book method to each member
+$member1 -> borrowBook ($book1);
+$member2 -> borrowBook ($book2);
+ 
+// TODO: Print Available Copies with their names:
+
+echo "</br>";
+// echo "Available Copies Of ' ".$book1->getTitle()." ':" .$book1->getAvailableCopies(). "</br>";
+
+echo "Available copies of- '" .$book1->getTitle(). "' : " . $book1-> getAvailableCopies () . "</br>";
+echo "Available copies of- '" . $book2->getTitle() . "' : " . $book2-> getAvailableCopies () . "</br>";
+
+//Available Copies of 'The Great Gatsby': 4
+//Available Copies of 'To Kill a Mockingbird': 2
+ 
+?>
---
+++ string.php
@@ -0,0 +1,38 @@
+<?php
+
+/*1. Count the number of vowels (a, e, i, o, u) in each string.
+2. Reverse each string.
+3. Print the original string and the modified strings along with their vowel counts.
+
+You must use this array of strings:
+$strings = ["Hello", "World", "PHP", "Programming"];
+
+Your Output must look like this:
+Original String: Hello, Vowel Count: 2, Reversed String: olleH
+
+*/
+
+
+$strings = ["Hello", "World", "PHP", "Programming"];
+
+function vowelCount ($strings){
+    $vowels= ['a', 'e', 'i', 'o', 'u'];
+    $count= 0;
+
+    for ($i=0; $i < strlen($strings); $i++){
+        if (in_array ($strings[$i], $vowels)){
+            $count++;
+        }
+
+    }
+    return $count;
+}
+
+foreach ($strings as $item){
+    $numbers_of_vowel = vowelCount ($item);
+    $reverse = strrev($item);
+    echo "Original String: " . $item. ", Vowel count: " . $numbers_of_vowel . ", Reversed String: " . $reverse. "</br>";
+}
+
+
+?>
